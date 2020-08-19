1817371e2c9d4de72c48e08f450a9182997931f5
greyhoundforty <ryantiffany@fastmail.com>
2020-08-19 12:55:21 -0500

    Fixed vlan for compute nodes
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- data.tf
+++ data.tf
@@ -3,9 +3,11 @@ data "ibm_compute_ssh_key" "deploymentKey" {
 }
 
 data "ibm_network_vlan" "public_vlan" {
-  number = var.public_vlan
+  number          = var.public_vlan
+  router_hostname = "fcr01a.wdc07"
 }
 
 data "ibm_network_vlan" "private_vlan" {
-  number = var.private_vlan
+  number          = var.private_vlan
+  router_hostname = "bcr01a.wdc07"
 }
